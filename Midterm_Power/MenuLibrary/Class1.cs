5d67bd4dcb1e4ca424e0ba1b262a2d4c38af15e6
RumiChakravarty <rumi.sarma.c@gmail.com>
2016-09-17 12:50:19 -0400

    Not the Final Project we are half way there.

---
+++ Midterm_Power/MenuLibrary/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSTerminal
+{
+    public class Menu
+    {
+        public string letter ;
+        public string name = "";
+        public string category = "";
+        public float price = 0.0f;
+
+        public Menu(string Letter, string Name, string Category, float Price)
+        {
+            this.letter = Letter;
+            this.name = Name;
+            this.category = Category;
+            this.price = Price;
+        }
+
+        //public string Name
+        //{
+        //    get { return Name; }
+        //}
+        //public string Category
+        //{
+        //    get { return Category; }
+        //}
+        //public float Price
+        //{
+        //    get { return Price; }
+        //}
+
+    }
+}
