9d19e1ed5b67d4c45acd8a1cfab32af7fdb499ae
Mo Yang <moyang9192@gmail.com>
2016-09-20 03:38:07 -0400

    Update Cart.cs

--- Midterm_Codesters/Cart.cs
+++ Midterm_Codesters/Cart.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,6 @@ namespace point_of_sale_midterm
 {
     public class Cart
     {
-        //public string select,item;
-        //public string quantity;
-        //public string line,total;
         List<cartItem> usersCart = new List<cartItem>();
     }
 }
