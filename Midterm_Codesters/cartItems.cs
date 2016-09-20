c30a5adb1b0d77815c74869b88242a5eec1d1d8a
Mo Yang <moyang9192@gmail.com>
2016-09-20 03:44:43 -0400

    Update cartItems.cs

--- Midterm_Codesters/cartItems.cs
+++ Midterm_Codesters/cartItems.cs
@@ -1,11 +1,9 @@
-﻿namespace point_of_sale_midterm
+namespace point_of_sale_midterm
 {
     public class cartItem
     {
         private Product item = new Product();
         private int quantity;
-        //private double subtotal;
-
         public double Subtotal { get { return item.Price * quantity; } }
 
         public int Quantity
@@ -19,6 +17,5 @@
             get { return item; }
             set { item = value; }
         }
-
     }
 }
